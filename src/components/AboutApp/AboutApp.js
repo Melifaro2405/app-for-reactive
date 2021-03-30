b4f37ffa0b1d1ab5cb87c0a7850a720e0970c12c
Denis <tomelifaro@yandex.ru>
2021-03-30 08:21:41 +0300

    changes for best semantic

--- src/components/AboutApp/AboutApp.js
+++ src/components/AboutApp/AboutApp.js
@@ -7,26 +7,33 @@ const AboutApp = () => {
     <section className='about-app'>
       <h2>Версия приложения 1.0.42</h2>
       <div className='about-app__description'>
-        <div className='about-app__description--technologies'>
-          <p>Использован следующий стек:</p>
-          <span>- HTML / CSS / ReactJS</span>
-          <span>- react-redux</span>
-          <span>- redux-devtools-extension</span>
-          <span>- redux-thunk</span>
-          <span>- react-router-dom</span>
-          <span>- react-hooks / react-redux-hooks</span>
-          <span>- axios / async-await / try-catch</span>
+
+        <div  className='about-app__description--technologies-list'>
+          <h3>Использован следующий стек:</h3>
+          <ul>
+            <li>- HTML / CSS / ReactJS</li>
+            <li>- react-redux</li>
+            <li>- redux-devtools-extension</li>
+            <li>- redux-thunk</li>
+            <li>- react-router-dom</li>
+            <li>- react-hooks / react-redux-hooks</li>
+            <li>- axios / async-await / try-catch</li>
+          </ul>
         </div>
-        <div className='about-app__description--functional'>
-          <p>Описание функционала:</p>
-          <span>В приложении реализовано получение данных с jsonplaceholder.typicode.com
-            в виде списка пользователей, а так же постов к ним по userId.</span>
-          <span>У пользователя можно поменять поле "username".</span>
-          <span>Реализовано удаление и добавление единиц списка.</span>
-          <span>Все действия проходят через redux actions. Настроены Redux devTools.</span>
-          <span>Проверено на переполнение контента.</span>
-          <span>Протестировано в браузерах Chrome v89, Safari v14, Firefox v85</span>
+
+        <div className='about-app__description--functional-list'>
+          <h3>Описание функционала:</h3>
+          <ul>
+            <li>В приложении реализовано получение данных с jsonplaceholder.typicode.com
+              в виде списка пользователей, а так же постов к ним по userId.</li>
+            <li>У пользователя можно поменять поле "username".</li>
+            <li>Реализовано удаление и добавление единиц списка.</li>
+            <li>Все действия проходят через redux actions. Настроены Redux devTools.</li>
+            <li>Проверено на переполнение контента.</li>
+            <li>Протестировано в браузерах Chrome v89, Safari v14, Firefox v85</li>
+          </ul>
         </div>
+
       </div>
       <p>Ссылка на GitHub (исходный код приложения):</p>
       <a href='https://github.com/Melifaro2405/app-for-reactive' target='_blank' rel="noreferrer">
